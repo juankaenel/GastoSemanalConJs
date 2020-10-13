86c5a54d6558bd8241fe7313ffbda4b27bfcfccc
Juan Kaenel <kaenel10@gmail.com>
2020-10-12 21:39:56 -0300

    Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -5,22 +5,40 @@ const gastoListado = document.querySelector('#gastos ul');
 
 //--------- Eventos ---------
 eventListeners();
-function eventListeners(){
-    document.addEventListener('DOMContentLoaded', preguntarPresupuesto());
+
+function eventListeners() {
+    document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 
 
 //--------- Clases ---------
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
 
+class UI {
 
+}
+
+//Instancia
+const ui = new UI();
+let presupuesto;
 
 //--------- Funciones ---------
-function preguntarPresupuesto(){
+function preguntarPresupuesto() {
+
     const presupuestoUsuario = prompt('¿Cuál es el presupuesto?');
-    console.log(presupuestoUsuario);
+    //console.log(presupuestoUsuario);
 
-
-    if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){ //Si está vacio,es null, o no es un NaN, o es un num negativo-> recargar el prompt
+    if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) { //Si está vacio,es null, o no es un NaN, o es un num negativo-> recargar el prompt
         window.location.reload();
     }
+    //validar presupuesto
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+
 }
